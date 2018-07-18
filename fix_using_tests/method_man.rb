dcf1a60994ee66ee6b9e3e4fa0ee0724f5910242
Wangtron <evans.wang@flatironschool.com>
2018-07-18 16:08:37 -0400

    solution

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,9 +1,9 @@
 # don't forget to add: require 'pry'
 
 def start_game(player1, player2)
-  "Hello #{player1} & #{playar2}"
+  "Hello #{player1} & #{player2}"
 end
 
-def play_game(player2)
+def play_game(player1, player2)
   "#{player1} is better than #{player2}"
 end
