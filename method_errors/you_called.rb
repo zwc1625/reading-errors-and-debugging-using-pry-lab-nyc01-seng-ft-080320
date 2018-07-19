f7c5c16379fadbb5ccdbe396f09b9b8a22e54065
gonzodamus <github email address>
2018-07-19 11:13:48 -0400

    changed up you_called to not break the entire thing.  Not sure if that's the right way to go or not yet

--- method_errors/you_called.rb
+++ method_errors/you_called.rb
@@ -1,7 +1,22 @@
 
 
-def super_mario_party
+
+def mario
+  "It's a me, Mario!"
+end
+
+def luigi
+  "Woo-hooooooo!"
+end
+
+def waluigi
+  "Waaaaaaagh"
+end
+
+def mario_party
   "Sorry, your princess is in another castle"
 end
 
-super_mario_party
+def super_mario_world
+  "erauqs si dlrow eht"
+end
--- spec/you_called_spec.rb
+++ spec/you_called_spec.rb
@@ -2,7 +2,7 @@ require 'pry'
 require_relative '../method_errors/you_called'
 
 describe "Mario Party" do
-  it "should call the method after it has been defined" do
+  it "should call the super_mario_party method and return a string" do
     expect(super_mario_party).to eq("Sorry, your princess is in another castle")
   end
 end
