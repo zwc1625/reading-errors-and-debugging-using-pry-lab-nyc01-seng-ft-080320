3d316642caa83fdf47b28f3ecd33077b4ee5eca6
gonzodamus <github email address>
2018-07-25 16:24:43 -0400

    clarified test names

--- spec/1.that_was_unexpected_spec.rb
+++ spec/1.that_was_unexpected_spec.rb
@@ -1,8 +1,8 @@
 require_relative '../first_priority/that_was_unexpected'
 
-describe "expect the unexpected" do
+describe "that_was_unexpected" do
   let(:baller) {Person.new('Jordan', true)}
   it "should tell me if the Person is a ballin candy lover" do
-    expect(baller.ballin_candy_lover?).to eq("Aw sweet, this cat love them some candy!")
+    expect(baller.ballin_candy_lover?).to eq("Aw sweet, this cat loves them some candy!")
   end
 end
