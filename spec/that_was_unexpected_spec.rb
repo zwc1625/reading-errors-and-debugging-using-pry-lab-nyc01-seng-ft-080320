c6254495173e4179053e14b50e614b0d93a8f9d8
gonzodamus <github email address>
2018-07-25 15:18:13 -0400

    fixed unexpected spec

--- spec/that_was_unexpected_spec.rb
+++ spec/that_was_unexpected_spec.rb
@@ -1,8 +1,8 @@
 require_relative '../first_priority/that_was_unexpected'
 
 describe "expect the unexpected" do
+  let(:baller) {Person.new('Jordan')}
   it "should tell me if the Person is a ballin candy lover" do
-    let(:baller) {Person.new('Jordan')}
     expect(baller.ballin_candy_lover?).to eq(true)
   end
 end
