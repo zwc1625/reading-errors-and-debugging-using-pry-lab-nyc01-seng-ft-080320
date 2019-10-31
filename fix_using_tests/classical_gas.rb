6c3bfc2f905f9701bb3e10628a0470aec332c528
Steven G. Harms <sgharms@stevengharms.com>
2019-10-31 09:43:57 -0400

    Hint for debugging issues around attr_ macros
    Fixes #1

--- fix_using_tests/classical_gas.rb
+++ fix_using_tests/classical_gas.rb
@@ -1,5 +1,9 @@
 class GasStation
 
+  # Remember which methods this "magic command", "attr_reader" creates.
+  # It writes "get" for these two instance variables.
+  # https://www.rubyguides.com/2018/11/attr_accessor/
+  # https://stackoverflow.com/questions/20018784/attr-accessor-vs-attr-reader-instance-variables/20018831
   attr_reader :brand, :unleaded_price
 
   def initialize(brand, unleaded_price)
