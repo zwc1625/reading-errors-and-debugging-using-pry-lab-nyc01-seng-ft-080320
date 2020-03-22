63d56cab7a2828850da779e1b7d3b7ff41e1a662
maxwellbenton <maxwellbenton@gmail.com>
2020-03-22 19:13:08 -0400

    update test for new task

--- spec/the_final_frontier_spec.rb
+++ spec/the_final_frontier_spec.rb
@@ -2,6 +2,6 @@ require_relative '../fix_using_tests/the_final_frontier'
 
 describe "the_final_frontier" do
   it "`engage` should return the captain's log and crew greetings" do
-    expect(engage).to eq(["Hello Geordi.", "Hello Data.", "Hello Worf.", "Hello William.", "Hello Beverly.", "Hello Deanna."])
+    expect{ engage }.to output(/Captain's Log, star date /).to_stdout
   end
 end
