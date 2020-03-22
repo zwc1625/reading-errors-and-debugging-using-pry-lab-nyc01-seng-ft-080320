943d8d1bbc49bca5604a49b541a2f3153b8cdd03
maxwellbenton <maxwellbenton@gmail.com>
2020-03-22 19:10:28 -0400

    update solution

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -2,7 +2,7 @@
 
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
+    's' * 10 + string
   else
     string
   end
--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -10,6 +10,6 @@ def state_log(star_date)
 end
 
 def engage
-  puts state_log(date)
   date = generate_star_date
+  puts state_log(date)
 end
